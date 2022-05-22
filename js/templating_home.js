26357e3dac844df32b6fd0acb005c80ed927bfb7
AdigunaSuryo7 <suryo@nubela.co>
2022-05-23 00:32:30 +0700

    initial commit

---
+++ js/templating_home.js
@@ -0,0 +1,124 @@
+let product_detail = [
+    {
+        "kategori": "minuman",
+        "img_src": "kopi.jpg,kopi2.jpg",        
+        "judul": "Kopi Salak Wedi",
+        "desc": "Bahan utama dari kopi ini adalah dari biji buah salak Wedi. Kopi ini cocok untuk orang-orang yang pingin minum kopi tapi rendah kafein. Untuk rasa memiliki sedikit rasa asam sehingga cocok untuk diminum pada pagi atau siang hari."
+    },
+    {
+        "kategori": "minuman",
+        "img_src": "sirup.jpg,sirup2.jpg",        
+        "judul": "Sirup Salak Wedi",
+        "desc": "Bahan utama dari sirup ini adalah dari sari buah salak Wedi. Rasa dari Sirup ini punya rasa salak yang cukup pekat dan khas seperti meminum teh. Sirup ini sangat cocok jika ditambah dengan lemon atau jeruk nipis sehingga bisa menemani saat cuaca lagi panas."
+    },
+    {
+        "kategori": "makanan-olahan",
+        "img_src": "asinan.jpg,asinan2.jpg",        
+        "judul": "Asinan Salak Wedi",
+        "desc": "Bahan utama dari asinan buah ini adalah buah salak Wedi. Makanan ini memiliki kuah yang asam, pedas, dan segar. Perbedaan dari asinan buah lainnya adalah pada asinan ini tidak terlalu banyak rasa dari cuka dan adanya rasa manis pada buah salak Wedi."
+    },
+    {
+        "kategori": "makanan-olahan",
+        "img_src": "kurma.jpg,kurma2.jpg",        
+        "judul": "Kurma Salak Wedi",
+        "desc": "Kurma salak Wedi adalah product best seller dari pusol Bunda Arum. Bahan utama dari kurma ini adalah buah salak Wedi. Dibanding dengan kurma pada umumnya yang memiliki rasa manis yang pekat rasa dari kurma salak Wedi adalah tidak terlalu manis dan sedikit memiliki rasa asam. Untuk tekstur dan bentuk kurma salak Wedi sama dengan kurma pada umumnya."
+    },
+    {
+        "kategori": "makanan-olahan",
+        "img_src": "dodol.jpg,dodol2.jpg",        
+        "judul": "Dodol Salak Wedi",
+        "desc": "Memiliki warna hitam kecoklatan, berbentuk lenjer, dan tekstur kenyal seperti dodol Betawi, tetapi untuk segi rasa dodol salak Wedi tidak terlalu manis dan tidak bikin enek."
+    },
+    {
+        "kategori": "makanan-ringan",
+        "img_src": "madumongso.jpg,madumongso2.jpg",        
+        "judul": "Madumongso Salak Wedi",
+        "desc": "Bahan utama dari Madumongso salak Wedi adalah sari buah salak wedi yang dicampur dengan ketan hitam. Rasa dari Madumongso salak Wedi tidak terlalu manis, memiliki tekstur yang chewy, dan aroma farmentasi dari ketan hitam tersebut."
+    },
+    {
+        "kategori": "makanan-ringan",
+        "img_src": "molen.jpg,molen2.jpg",        
+        "judul": "Molen Salak Wedi",
+        "desc": "Mungkin Molen pisang sudah sering ditemukan, tetapi untuk product molen kering ini berisi isian dari olahan kurma salak wedi yang berukuran lebih kecil sehingga dapat dibalut dengan kulit molen."
+    },
+    {
+        "kategori": "merchandise",
+        "img_src": "mug1.jpg",        
+        "judul": "Mug Tropical Fruits",
+        "desc": "Tipe Mug bisa digunakan untuk minuman panas atau dingin seperti kopi, tea, coklat panas, dan soda. Memiliki handle agar nyaman dipegang, dan dapat menampung hingga 240-350ml. Mug ini menggunakan bahan dasar keramik yang tergolong bahan isolator."
+    },
+    {
+        "kategori": "merchandise",
+        "img_src": "mug2.jpg",        
+        "judul": "Mug Growing Seed",
+        "desc": "Tipe Mug bisa digunakan untuk minuman panas atau dingin seperti kopi, tea, coklat panas, dan soda. Memiliki handle agar nyaman dipegang, dan dapat menampung hingga 240-350ml. Mug ini menggunakan bahan dasar keramik yang tergolong bahan isolator."
+    },
+    {
+        "kategori": "merchandise",
+        "img_src": "mug3.jpg",        
+        "judul": "Mug Snake Fruit Palm",
+        "desc": "Tipe Mug bisa digunakan untuk minuman panas atau dingin seperti kopi, tea, coklat panas, dan soda. Memiliki handle agar nyaman dipegang, dan dapat menampung hingga 240-350ml. Mug ini menggunakan bahan dasar keramik yang tergolong bahan isolator."
+    },
+    {
+        "kategori": "merchandise",
+        "img_src": "tshirt5.jpg,tshirt1.jpg",        
+        "judul": "T-Shirt Snake Fruit Season",
+        "desc": "Product kaos ini memiliki design terkait dengan gambar 'salak'. Tersedia dalam ukuran small, medium,extra large, dan triple extra large. Bahan dari kaos adalah jenis kain cotton bamboo yang dimana memiliki keunggulan anti bakteri, dapat menyerap keringat dengan baik, dan ramah lingkungan."
+    },    
+    {
+        "kategori": "merchandise",
+        "img_src": "tshirt8.jpg,tshirt2.jpg",        
+        "judul": "T-Shirt Life On Bojonegoro",
+        "desc": "Product kaos ini memiliki design terkait dengan gambar 'salak'. Tersedia dalam ukuran small, medium,extra large, dan triple extra large. Bahan dari kaos adalah jenis kain cotton bamboo yang dimana memiliki keunggulan anti bakteri, dapat menyerap keringat dengan baik, dan ramah lingkungan."
+    },
+    {
+        "kategori": "merchandise",
+        "img_src": "tshirt6.jpg,tshirt3.jpg",        
+        "judul": "T-Shirt Growing Seed",
+        "desc": "Product kaos ini memiliki design terkait dengan gambar 'salak'. Tersedia dalam ukuran small, medium,extra large, dan triple extra large. Bahan dari kaos adalah jenis kain cotton bamboo yang dimana memiliki keunggulan anti bakteri, dapat menyerap keringat dengan baik, dan ramah lingkungan."
+    },
+    {
+        "kategori": "merchandise",
+        "img_src": "tshirt7.jpg,tshirt4.jpg",        
+        "judul": "T-Shirt Plant, Grow, and Care",
+        "desc": "Product kaos ini memiliki design terkait dengan gambar 'salak'. Tersedia dalam ukuran small, medium,extra large, dan triple extra large. Bahan dari kaos adalah jenis kain cotton bamboo yang dimana memiliki keunggulan anti bakteri, dapat menyerap keringat dengan baik, dan ramah lingkungan."
+    },    
+    {
+        "kategori": "merchandise",
+        "img_src": "totebag1.jpg",        
+        "judul": "Tote Bag Snake Fruit Palm ",
+        "desc": "Tas jinjing simple yang dilengkapi dengan dua tali pegangan pada bagian atas. Totebag ini memiliki design gambar terkait dengan 'salak', ukuran tote bag mulai dari ukuran sedang hingga besar. Tote bag dapat difungsikan untuk membawa sejumlah barang misalnya untuk berbelanja atau perkuliahan. Bahan yang digunakan terbuat dari kain kanvas."
+    },
+    {
+        "kategori": "merchandise",
+        "img_src": "totebag2.jpg",        
+        "judul": "Tote Bag Snake Fruit",
+        "desc": "Tas jinjing simple yang dilengkapi dengan dua tali pegangan pada bagian atas. Totebag ini memiliki design gambar terkait dengan 'salak', ukuran tote bag mulai dari ukuran sedang hingga besar. Tote bag dapat difungsikan untuk membawa sejumlah barang misalnya untuk berbelanja atau perkuliahan. Bahan yang digunakan terbuat dari kain kanvas."
+    },
+]
+
+for(let i = 0; i < product_detail.length; i++){   
+    let img_list = product_detail[i]["img_src"].split(",")
+    let query = "kategori=" + product_detail[i]["kategori"] + "&img_src=" + product_detail[i]["img_src"] + "&judul=" + product_detail[i]["judul"] + "&desc=" + product_detail[i]["desc"]
+    let minuman_product = 
+    `<div class="col-lg-3 col-md-4 col-sm-6 mix ${product_detail[i]["kategori"]}">
+        <div class="featured__item">
+            <div class="featured__item__pic set-bg" data-setbg="img/product/${img_list[0]}">
+                <ul class="featured__item__pic__hover">
+                    <li><a href="shop-details.html?${btoa(query)}"><i class="fa fa-info"></i></a></li>                    
+                </ul>
+            </div>
+            <div class="featured__item__text">
+                <h6><a href="shop-details.html?${btoa(query)}">${product_detail[i]["judul"]}</a></h6>                            
+            </div>
+        </div>
+    </div>`;
+    document.getElementById("list_product").insertAdjacentHTML("beforeend", minuman_product)       
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
